9c4c56736bbb469b2af814a0fdafc66bc696a665
stewie <stewiemayer@hotmail.com>
2025-12-04 13:36:29 -0600

    feat: se elimina el header de este nuvel y se agrega el Outlet de react-router

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,18 +1,17 @@
-import React, { PropsWithChildren } from "react";
-import Header from "./Header";
-import Footer from "./Footer";
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Header from './Header';
+import Footer from './Footer';
 
-interface LayoutProps extends PropsWithChildren {
-  headerContent: React.ReactNode;
-}
-
-const Layout: React.FC<LayoutProps> = ({ children, headerContent }) => {
+const Layout: React.FC = () => {
   return (
     <div className="flex flex-col min-h-screen">
-      <Header>{headerContent}</Header>
-      <main className="grow p-4">{children}</main>
+      <main className="grow p-4">
+        <Outlet />
+      </main>
       <Footer />
     </div>
   );
 };
+
 export default Layout;
