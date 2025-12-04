63076aa90a3325bb49fa837452d83e2d1141ca4f
stewie <stewiemayer@hotmail.com>
2025-12-04 13:46:09 -0600

    feat: se elimina el main para permitir el control del contexto en outlet

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,12 +1,10 @@
-import { Outlet } from 'react-router-dom';
-import Footer from './Footer';
+import { Outlet } from "react-router-dom";
+import Footer from "./Footer";
 
 const Layout: React.FC = () => {
   return (
     <div className="flex flex-col min-h-screen">
-      <main className="grow p-4">
-        <Outlet />
-      </main>
+      <Outlet />
       <Footer />
     </div>
   );
