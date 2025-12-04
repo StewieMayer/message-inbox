b2b4172b3ad410533dd903bebae49d02ae8c33fc
stewie <stewiemayer@hotmail.com>
2025-12-04 15:37:10 -0600

    feat: se crea un slice para los threads y se agrega al store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,8 +1,11 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore } from "@reduxjs/toolkit";
+import threadReducer from "./features/threadsSlice";
 
 export const store = configureStore({
-  reducer: {},
-})
+  reducer: {
+    threads: threadReducer,
+  },
+});
 
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
