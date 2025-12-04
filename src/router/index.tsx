41ec04ce95d510acab4e97d01dcbb1504fa6eb19
stewie <stewiemayer@hotmail.com>
2025-12-04 15:38:39 -0600

    feat: se modifica para que tanto los public y los protected usen el mismo layout

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,28 +1,28 @@
-import { createBrowserRouter, Navigate } from 'react-router-dom';
-import LoginPage from '../pages/login/LoginPage';
-import InboxPage from '../pages/inbox/InboxPage';
-import ProtectedRoute from './ProtectedRoute';
-import Layout from '../components/Layout';
+import { createBrowserRouter, Navigate } from "react-router-dom";
+import LoginPage from "../pages/login/LoginPage";
+import InboxPage from "../pages/inbox/InboxPage";
+import ProtectedRoute from "./ProtectedRoute";
+import Layout from "../components/Layout";
 
 export const router = createBrowserRouter([
   {
-    path: '/',
-    element: <LoginPage />,
-  },
-  {
-    path: '/app',
-    element: <ProtectedRoute />,
+    path: "/",
+    element: <Layout />,
     children: [
       {
-        path: '/app',
-        element: <Layout />,
+        index: true,
+        element: <LoginPage />,
+      },
+      {
+        path: "/app",
+        element: <ProtectedRoute />,
         children: [
           {
             index: true,
             element: <Navigate to="/app/inbox" replace />,
           },
           {
-            path: 'inbox',
+            path: "inbox",
             element: <InboxPage />,
           },
         ],
