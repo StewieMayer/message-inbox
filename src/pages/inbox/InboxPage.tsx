debf4e5e79c575e30d977d4582f2c4a48adf1a41
stewie <stewiemayer@hotmail.com>
2025-12-04 18:58:49 -0600

    feat: se crean la UI de los componentes del inbox

--- src/pages/inbox/InboxPage.tsx
+++ src/pages/inbox/InboxPage.tsx
@@ -1,6 +1,19 @@
+import InboxHeader from "./components/inbox-header/InboxHeader";
+import MessageModal from "./components/message-modal/MessageModal";
+import ThreadContainer from "./components/thread-container/ThreadContainer";
+import ThreadViewer from "./components/thread-viewer/ThreadViewer";
 
 const InboxPage: React.FC = () => {
-  return <></>;
+  return (
+    <div className="flex flex-col grow">
+      <InboxHeader />
+      <main className="flex grow p-5 gap-5 bg-blue-100">
+        <ThreadContainer />
+        <ThreadViewer />
+      </main>
+      <MessageModal />
+    </div>
+  );
 };
 
 export default InboxPage;
---
+++ src/pages/inbox/components/inbox-header/InboxHeader.tsx
@@ -0,0 +1,24 @@
+import Header from "../../../../components/Header";
+import SearchBar from "./components/search-bar/SearchBar";
+import { Mail } from "lucide-react";
+
+const InboxHeader: React.FC = () => {
+  return (
+    <Header>
+      <div className="flex justify-between items-center">
+        <div className="flex gap-2 items-center rounded py-1 px-2 border-b-cyan-800 border-b-2 border-r-cyan-600 border-r-2">
+          <Mail className="h-8 w-8 border rounded px-1" />
+          <h1 className="text-3xl font-extrabold text-gray-100">MiInbox</h1>
+        </div>
+        <div className="flex gap-4">
+          <SearchBar />
+          <button className="bg-white text-cyan-700 px-4 py-2 rounded hover:bg-gray-200 hover:cursor-pointer">
+            New Message
+          </button>
+        </div>
+      </div>
+    </Header>
+  );
+};
+
+export default InboxHeader;
